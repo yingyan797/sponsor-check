2c2574a7afa3b9da0a27064f30ca692bcafaf3af
Andy Chen <andy.chen121@imperial.ac.uk>
2024-12-14 17:47:32 +0000

    setup

---
+++ nlp.js
@@ -0,0 +1,11 @@
+// import { pipeline } from '@huggingface/transformers';
+// import { pipeline } from "https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.1.2"
+alert(1)
+try {
+    const classifier = await pipeline('sentiment-analysis');
+    const output = await classifier('I love transformers!');
+
+    console.log(output);
+} catch (error) {
+    console.log(error)
+}
